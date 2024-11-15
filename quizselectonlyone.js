74adf4aca7a95768583c480199b0057287e65a4e
sana <sana.sett82@gmail.com>
2024-11-15 17:49:49 +0330

    quiz questions

--- quizselectonlyone.js
+++ quizselectonlyone.js
@@ -1,7 +1,7 @@
 function selectOnlyOne(selectedCheckbox, questionId) {
     const checkboxes = document.querySelectorAll(`input[type="checkbox"][id^="${questionId}"]`);
 
-    // Uncheck all checkboxes except the one clicked
+    // Uncheck all checkboxes except the one clicked0
     checkboxes.forEach((checkbox) => {
         if (checkbox !== selectedCheckbox) {
             checkbox.checked = false;
